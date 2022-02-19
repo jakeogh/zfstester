40e79aa33cc6365c149b552a92991b6f1ab5014c
jakeogh <github.com@v6y.net>
2022-02-19 15:48:09 -0700

    auto-commit

--- zfstester/zfstester.py
+++ zfstester/zfstester.py
@@ -26,35 +26,32 @@ import time
 import uuid
 from math import inf
 from pathlib import Path
+from typing import Callable
+from typing import Optional
+from typing import Union
 
 import click
-#from with_chdir import chdir
 import sh
-#from getdents import paths
-from kcl.pathops import path_is_block_special
+from asserttool import ic
+# from with_chdir import chdir
+from clicktool import click_add_options
+from clicktool import click_global_options
+from clicktool import tv
 from pathstat import display_results
 from pathstat import pathstat
+# from getdents import paths
+from pathtool import path_is_block_special
 from run_command import run_command
 
 
-def eprint(*args, **kwargs):
-    if 'file' in kwargs.keys():
-        kwargs.pop('file')
-    print(*args, file=sys.stderr, **kwargs)
-
-
-try:
-    from icecream import ic  # https://github.com/gruns/icecream
-except ImportError:
-    ic = eprint
-
-
-def make_things(root, count, thing_function):
+def make_things(
+    root: Path, count: Optional[int], thing_function: Callable[[Path], None]
+) -> None:
     assert thing_function in [os.makedirs, os.mknod, os.symlink]
-    target = str(time.time())
-    target = Path(root) / Path(target)
+    timestamp = str(time.time())
+    target = root / Path(timestamp)
     os.makedirs(target)
-    if count != inf:
+    if count:
         for _ in range(count):
             thing_function(target / Path(uuid.uuid4().hex))
     else:
@@ -62,17 +59,16 @@ def make_things(root, count, thing_function):
             thing_function(target / Path(uuid.uuid4().hex))
 
 
-def check_df(match):
-    if isinstance(match, Path):
-        match = match.as_posix()
+def check_df(path: Path):
+    _path = path.as_posix()
     df_result = sh.df("-h").splitlines()
     found = False
     for line in df_result:
-        if match in line:
+        if _path in line:
             ic(line)
             found = True
     if not found:
-        raise ValueError("{} not in df -h output".format(match))
+        raise ValueError(f"{_path} not in df -h output")
 
 
 def cleanup_loop_device(device):
@@ -94,72 +90,86 @@ def destroy_zfs_pool(pool):
 
 
 @click.command()
-@click.argument("destination_folder",
-                type=click.Path(exists=True,
-                                dir_okay=True,
-                                file_okay=False,
-                                path_type=str,
-                                allow_dash=False),
-                nargs=1,
-                required=True)
-@click.option("--loop",
-              type=click.Path(exists=True,
-                              dir_okay=False,
-                              file_okay=True,
-                              path_type=str,
-                              allow_dash=False),
-              nargs=1,
-              required=False)
-@click.option('--verbose', is_flag=True)
-@click.option('--debug', is_flag=True)
-@click.option('--ipython', is_flag=True)
-@click.option('--record-count', type=int)
-@click.option('--zpool-size-mb', type=int, default=64)
-@click.option('--recordsize', type=str, default="128K")  # The size specified must be a power of two greater than or equal to 512 and less than or equal to 128 Kbytes man zprops
-@click.option("--printn", is_flag=True)
-def cli(destination_folder: str,
-        loop: str,
-        zpool_size_mb: int,
-        recordsize: str,
-        verbose: bool,
-        debug: bool,
-        record_count: int,
-        ipython: bool,
-        printn: bool,
-        ):
+@click.argument(
+    "destination_folder",
+    type=click.Path(
+        exists=True, dir_okay=True, file_okay=False, path_type=str, allow_dash=False
+    ),
+    nargs=1,
+    required=True,
+)
+@click.option(
+    "--loop",
+    type=click.Path(
+        exists=True, dir_okay=False, file_okay=True, path_type=str, allow_dash=False
+    ),
+    nargs=1,
+    required=False,
+)
+@click.option("--verbose", is_flag=True)
+@click.option("--ipython", is_flag=True)
+@click.option("--record-count", type=int)
+@click.option("--zpool-size-mb", type=int, default=64)
+@click.option(
+    "--recordsize", type=str, default="128K"
+)  # The size specified must be a power of two greater than or equal to 512 and less than or equal to 128 Kbytes man zprops
+@click_add_options(click_global_options)
+@click.pass_context
+def cli(
+    ctx,
+    destination_folder: str,
+    loop: str,
+    zpool_size_mb: int,
+    recordsize: str,
+    verbose: Union[bool, int, float],
+    verbose_inf: bool,
+    record_count: int,
+    ipython: bool,
+):
+
+    tty, verbose = tv(
+        ctx=ctx,
+        verbose=verbose,
+        verbose_inf=verbose_inf,
+    )
 
     if os.getuid() != 0:
-        ic('must be root')
+        ic("must be root")
         sys.exit(1)
 
     if zpool_size_mb < 64:
-        raise ValueError('minimum zpool size is 64MB')
+        raise ValueError("minimum zpool size is 64MB")
 
     timestamp = str(time.time())
     if verbose:
         ic(timestamp)
 
     if not loop:
-        loop = '/dev/loop0'
+        loop = "/dev/loop0"
 
     if not path_is_block_special(loop):
         raise ValueError("loop device path {} is not block special".format(loop))
 
     loops_in_use = sh.losetup("-l").splitlines()
-    #ic(loops_in_use)
+    # ic(loops_in_use)
     for line in loops_in_use:
         if loop in loops_in_use:
             raise ValueError("loop device {} already in use".format(loop))
 
-    destination = Path(destination_folder) / Path('zfstester_' + timestamp)
+    destination = Path(destination_folder) / Path("zfstester_" + timestamp)
     os.makedirs(destination)
 
     destination_pool_file = destination / Path("test_pool_{}".format(timestamp))
     if verbose:
         ic(destination_pool_file)
-    sh.dd("if=/dev/zero", "of={}".format(destination_pool_file), "bs={}M".format(zpool_size_mb), "count=1")
-    #dd if=/dev/urandom of=temp_zfs_key bs=32 count=1 || exit 1
-    #key_path=`readlink -f temp_zfs_key`
+    sh.dd(
+        "if=/dev/zero",
+        f"of={destination_pool_file.as_posix()}",
+        f"bs={zpool_size_mb}M",
+        "count=1",
+    )
+    # dd if=/dev/urandom of=temp_zfs_key bs=32 count=1 || exit 1
+    # key_path=`readlink -f temp_zfs_key`
 
     sh.losetup(loop, destination_pool_file, loop)
     atexit.register(cleanup_loop_device, loop)
@@ -169,33 +179,45 @@ def cli(destination_folder: str,
     zpool_name = destination_pool_file.name
     if verbose:
         ic(zpool_name)
-    zpool_create_command = ["zpool",
-                            "create",
-                            "-O", "atime=off",
-                            "-O", "compression=lz4",
-                            "-O", "mountpoint=none",
-                            "-O", "recordsize=" + recordsize,
-                            zpool_name,
-                            loop]
+    zpool_create_command = [
+        "zpool",
+        "create",
+        "-O",
+        "atime=off",
+        "-O",
+        "compression=lz4",
+        "-O",
+        "mountpoint=none",
+        "-O",
+        "recordsize={recordsize}",
+        zpool_name,
+        loop,
+    ]
     run_command(zpool_create_command, verbose=True)
-    #atexit.register(destroy_zfs_pool, zpool_name)
+    # atexit.register(destroy_zfs_pool, zpool_name)
 
-    zfs_mountpoint = "{}_mountpoint".format(destination_pool_file)
-    zfs_filesystem = "{}/spacetest".format(zpool_name)
-    zfs_create_command = ["zfs", "create", "-o", "mountpoint={}".format(zfs_mountpoint), "-o", "recordsize=" + recordsize, zfs_filesystem]
+    zfs_mountpoint = Path(f"{destination_pool_file.as_posix()}_mountpoint")
+    zfs_filesystem = f"{zpool_name}/spacetest"
+    zfs_create_command = [
+        "zfs",
+        "create",
+        "-o",
+        f"mountpoint={zfs_mountpoint.as_posix()}",
+        "-o",
+        "recordsize={recordsize}",
+        zfs_filesystem,
+    ]
     run_command(zfs_create_command, verbose=True)
-    #atexit.register(destroy_zfs_filesystem, zfs_filesystem)
+    # atexit.register(destroy_zfs_filesystem, zfs_filesystem)
     atexit.register(umount_zfs_filesystem, zfs_mountpoint)
 
-    ## disabled just for pure space tests
-    ##zfs create -o encryption=on -o keyformat=raw -o keylocation=file://"${key_path}" -o mountpoint=/"${destination_pool_file}"/spacetest_enc "${destination_pool_file}"/spacetest_enc || exit 1
+    # disabled just for pure space tests
+    # zfs create -o encryption=on -o keyformat=raw -o keylocation=file://"${key_path}" -o mountpoint=/"${destination_pool_file}"/spacetest_enc "${destination_pool_file}"/spacetest_enc || exit 1
 
     check_df(destination_pool_file)
 
     try:
-        make_things(root=zfs_mountpoint,
-                    count=inf,
-                    thing_function=os.makedirs)
+        make_things(root=zfs_mountpoint, count=None, thing_function=os.makedirs)
     except Exception as e:
         ic(e)
 
@@ -210,13 +232,16 @@ def cli(destination_folder: str,
     # 512  recordsize: 80894
 
     zfs_get_all_command = ["zfs", "get", "all"]
-    output = run_command(zfs_get_all_command).decode('utf8')
+    output = run_command(
+        zfs_get_all_command,
+        verbose=verbose,
+    ).decode("utf8")
     for line in output.splitlines():
         if destination_pool_file.name in line:
             print(line)
 
-    df_inodes = str(sh.df('-i'))
-    #ic(df_inodes)
+    df_inodes = str(sh.df("-i"))
+    # ic(df_inodes)
     print()
     for index, line in enumerate(df_inodes.splitlines()):
         if index == 0:
@@ -224,24 +249,37 @@ def cli(destination_folder: str,
         if destination_pool_file.name in line:
             print(line)
 
-
-    destination_pool_file_rzip = destination_pool_file.as_posix() + '.rz'
-    sh.rzip('-k', '-9', '-o', destination_pool_file_rzip, destination_pool_file.as_posix())
+    destination_pool_file_rzip = destination_pool_file.as_posix() + ".rz"
+    sh.rzip(
+        "-k", "-9", "-o", destination_pool_file_rzip, destination_pool_file.as_posix()
+    )
     compressed_file_size = os.stat(destination_pool_file_rzip).st_size
-    #ic(compressed_file_size)
+    # ic(compressed_file_size)
 
-    print('\nSummary:')
-    #ic(pathstat_results)
-    bytes_in_names = pathstat_results['bytes_in_names']
+    print("\nSummary:")
+    # ic(pathstat_results)
+    bytes_in_names = pathstat_results["bytes_in_names"]
     objects_created = pathstat_results[4]
-    print('Why did this {zpool_size_mb}MB pool run out of space?\nExactly {bytes_in_names} bytes were written to it by creating {objects_created} empty directories (with random uncompressable names) under the root of the zfs filesystem.\nCompressed, the pool file takes {compressed_file_size} bytes.'.format(zpool_size_mb=zpool_size_mb, compressed_file_size=compressed_file_size, bytes_in_names=bytes_in_names, objects_created=objects_created))
+    print(
+        "Why did this {zpool_size_mb}MB pool run out of space?\nExactly {bytes_in_names} bytes were written to it by creating {objects_created} empty directories (with random uncompressable names) under the root of the zfs filesystem.\nCompressed, the pool file takes {compressed_file_size} bytes.".format(
+            zpool_size_mb=zpool_size_mb,
+            compressed_file_size=compressed_file_size,
+            bytes_in_names=bytes_in_names,
+            objects_created=objects_created,
+        )
+    )
 
     compression_ratio = (compressed_file_size / (zpool_size_mb * 1024 * 1024)) * 100
-    print(str(round(compression_ratio, 2)) + 'X')
+    print(str(round(compression_ratio, 2)) + "X")
 
-    print("\nHow can more 64byte randomly named empty directories be created under the root of this {zpool_size_mb}MB pool?".format(zpool_size_mb=zpool_size_mb))
+    print(
+        "\nHow can more 64byte randomly named empty directories be created under the root of this {zpool_size_mb}MB pool?".format(
+            zpool_size_mb=zpool_size_mb
+        )
+    )
     if ipython:
         import IPython
+
         IPython.embed()
 
 
@@ -258,14 +296,13 @@ def cli(destination_folder: str,
 ##python3 -c "import os; import time; import uuid; target=str(time.time()); os.makedirs(target); os.chdir(target); [os.symlink(uuid.uuid4().hex, uuid.uuid4().hex) for _ in range($record_count)]" || exit 1
 #
 ## 64M -> 108465
-#python3 -c "import os; import time; import uuid; target=str(time.time()); os.makedirs(target); os.chdir(target); [os.symlink('None', uuid.uuid4().hex) for _ in range($record_count)]" || exit 1
+# python3 -c "import os; import time; import uuid; target=str(time.time()); os.makedirs(target); os.chdir(target); [os.symlink('None', uuid.uuid4().hex) for _ in range($record_count)]" || exit 1
 #
-#df -h | grep "${destination_pool_file}" || exit 1
-#/bin/ls -alh || exit 1
+# df -h | grep "${destination_pool_file}" || exit 1
+# /bin/ls -alh || exit 1
 #
 ## disabled for pure space tests
 ##cp -ar * /"${destination_pool_file}"/spacetest_enc/
 #
 #
-#df -h | grep "${destination_pool_file}"
-
+# df -h | grep "${destination_pool_file}"
